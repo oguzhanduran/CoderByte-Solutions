7a7f116aedd38fa8c07da53a9ef74b6d0b1e6ca5
OGUZHAN DURAN <oguzhnduran@gmail.com>
2021-11-12 11:46:32 +0300

    Simple Mode was Editted.

--- 2-Medium/3-Simple-Mode.js
+++ 2-Medium/3-Simple-Mode.js
@@ -1,26 +1,25 @@
 function SimpleMode(arr){ 
-
-    // code goes here  
-    var mode=0;
-    var count2=0;
-    for(var i =0; i< arr.length; i++){
-      var count=0;
-      for(var j = 0; j < arr.length; j++){
-        if(arr[i] == arr[j] && j!==i){
-          count++
+  let counter2 = "0";
+  let mode = "0";
+      // code goes here  
+    for (let i = 0; i < arr.length; i++) {
+        var counter1 = 0;
+      for (let j = 0;  j< arr.length; j++) {
+        if (arr[i] == arr[j] && j!==i) {
+          counter1++
         }
-        if(count>count2){
-          mode = arr[i];
-          count2=count;
-          count= 0;
+        if (counter1 > counter2) {
+            counter2 = counter1;
+            mode = arr[i];
+            counter1 = 0;
         }
       }
     }
-    if(count2==0){
-      return -1;
+    if (counter2 == 0) {
+      return "-1"
     }
-    return mode;
-  }
+    return mode
+   }
      
   // keep this function call here 
   console.log(SimpleMode(readline()));
